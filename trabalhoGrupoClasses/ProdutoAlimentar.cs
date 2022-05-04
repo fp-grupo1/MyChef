07beaf0f5b6ee9ff66a55e047ab69497856585d0
Tiago Prazeres <us.tprazeres@formacao.ips.pt>
2022-05-05 00:37:28 +0100

    t

--- trabalhoGrupoClasses/ProdutoAlimentar.cs
+++ trabalhoGrupoClasses/ProdutoAlimentar.cs
@@ -128,6 +128,7 @@ namespace trabalhoGrupoClasses
         // É implementado nas classes filhas, devolve se um produto é saudável
         public abstract bool ESaudavel();
 
+        public abstract string TipoToString();
         // Devolve o sucesso ou não do registo de um novo distribuidor,
         // incrementando o número de distribuidores se não ultrapassar o número máximo de distribuidores
         public bool RegistarDistribuidor()
